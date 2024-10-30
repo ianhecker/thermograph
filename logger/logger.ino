d2b528008cf17b197606114417f4dc9d71af375e
ian hecker <ian@blocky.rocks>
2024-10-30 14:19:33 -0600

    Feature[RTC logger] Add logging func

--- logger/logger.ino
+++ logger/logger.ino
@@ -1,11 +1,33 @@
-void setup(void) { pinMode(13, OUTPUT); }
+#include "RTClib.h"
+
+RTC_DS1307 rtc;
+
+void error(char *err) {
+  Serial.print("error: ");
+  Serial.println(err);
+}
+
+void log(DateTime now, char *msg) {
+  Serial.print(now.unixtime());
+  Serial.print(" ");
+  Serial.println(msg);
+}
+
+void setup(void) {
+  pinMode(13, OUTPUT);
+
+  if (!rtc.begin()) {
+    Serial.println("RTC failed");
+  }
+
+  if (!rtc.isrunning()) {
+    Serial.println("RTC is not running");
+  }
+}
 
 void loop(void) {
   Serial.begin(9600);
 
-  Serial.print("Hello World!\n");
-
-  digitalWrite(13, HIGH);
+  log(rtc.now(), "hello");
   delay(1000);
-  digitalWrite(13, LOW);
 }
