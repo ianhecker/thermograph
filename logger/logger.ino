9aa27b21064c439fda4dc6c2a7842a0b8a8070bc
ian hecker <ian@blocky.rocks>
2024-11-22 13:51:38 -0700

    Feature[Debug Logs] Add ability to log to serial, file, or both

--- logger/logger.ino
+++ logger/logger.ino
@@ -9,6 +9,12 @@
 
 RTC_DS1307 rtc;
 
+byte DEBUG_LOG_OUTPUT = 0b00;
+#define DEBUG_LOG_OFF = 0b00
+#define DEBUG_LOG_SERIAL 0b01
+#define DEBUG_LOG_FILE 0b10
+#define DEBUG_LOG_SERIAL_AND_FILE 0b11
+
 void error(char *err) {
   Serial.print("error: ");
   Serial.println(err);
@@ -29,18 +35,45 @@ void double_blink(int pin, int blink_ms, int delay_ms) {
 void notify_write() {
   double_blink(NOTIFY_LED, 200 * MILLISECOND, 200 * MILLISECOND);
 }
+
+String formatDebug(DateTime now, String ctx, float temp) {
+  char buffer[100];
+  char tempBuffer[10];
+  dtostrf(temp, 6, 2, tempBuffer);
+
+  char fmt[50] = "unix:'%lu' | context:'%s' | temperature:'%s'";
+  sprintf(buffer, fmt, now.unixtime(), ctx.c_str(), tempBuffer);
+
+  return String(buffer);
+}
+
+void logSerial(String log) { Serial.println(log); }
+
+void logFile(String log) {
+  // LOG TO SD STUB
+  Serial.println(log);
+}
+
+void debug(String msg) {
+
+  if (DEBUG_LOG_OUTPUT & DEBUG_LOG_SERIAL) {
+    logSerial(msg);
+  }
+  if (DEBUG_LOG_OUTPUT & DEBUG_LOG_FILE) {
+    logFile(msg);
+  }
 }
 
 float getTemperature() {
-  float temperature = 72.23;
-  log(rtc.now(), "took temperature:" + String(temperature) + " degrees");
+  // TEMPERATURE SENSOR STUB
+
+  float temperature = 101.234;
 
   return temperature;
 }
 
 void writeTemperature(float temperature) {
-  log(rtc.now(), "wrote temperature:" + String(temperature) + " to memory");
-  return;
+  // SD CARD WRITE STUB
 }
 
 void setup(void) {
@@ -53,15 +86,25 @@ void setup(void) {
   if (!rtc.isrunning()) {
     Serial.println("RTC is not running");
   }
+
+  DEBUG_LOG_OUTPUT = DEBUG_LOG_SERIAL;
 }
 
 void loop(void) {
   Serial.begin(9600);
+  String dbg;
 
   float temperature = getTemperature();
 
-  writeTemperature(temperature);
-  notify();
+  dbg = formatDebug(rtc.now(), "took temp", temperature);
+  debug(dbg);
 
-  delay(1 * SECOND);
+  writeTemperature(temperature);
+
+  dbg = formatDebug(rtc.now(), "wrote temp to SD card", temperature);
+  debug(dbg);
+
+  // notify_write();
+
+  delay(5 * SECOND);
 }
