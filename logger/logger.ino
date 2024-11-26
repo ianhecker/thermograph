9047c98197ae2d83b060e87a648f02b3bea637be
ian hecker <ian@blocky.rocks>
2024-11-26 12:19:37 -0700

    Feature[Logging] Refactor out buffer formatting to make logging simpler

--- logger/logger.ino
+++ logger/logger.ino
@@ -9,7 +9,10 @@
 #define MINUTE 60 * SECOND
 #define LOG_INTERVAL 2 * SECOND
 
-RTC_DS1307 rtc;
+RTC_PCF8523 rtc;
+
+char DAYS_OF_THE_WEEK[7][12] = {"Sunday",   "Monday", "Tuesday", "Wednesday",
+                                "Thursday", "Friday", "Saturday"};
 
 // DEBUG LOGS ------------------------------------------------------------------
 byte DEBUG_LOG_OUTPUT = 0b00;
@@ -49,27 +52,66 @@ void LogFile(String log) {
   Serial.println(log);
 }
 
+// Data Formatting -------------------------------------------------------------
+struct fmt {
+
+  String RTCToHumanReadable(DateTime dt) {
+    char buffer[100];
+    char fmt[50] = "%d/%02d/%02d %s %02d:%02d:%02d";
+
+    char *dayOfTheWeek = DAYS_OF_THE_WEEK[dt.dayOfTheWeek()];
+
+    sprintf(buffer, fmt, dt.year(), dt.month(), dt.day(), dayOfTheWeek,
+            dt.hour(), dt.minute(), dt.second());
+
+    return String(buffer);
+  }
+
+  String TimestampAndDate(DateTime time) {
+    char buffer[50];
+    char fmt[30] = "unix:'%lu' | date:'%s'";
+
+    sprintf(buffer, fmt, time.unixtime(), RTCToHumanReadable(time).c_str());
+
+    return String(buffer);
+  }
+
+  String Context(String ctx) {
+    char buffer[50];
+    char fmt[25] = " | context:'%s'";
+
+    sprintf(buffer, fmt, ctx.c_str());
+
+    return String(buffer);
+  }
+
+  String FloatToString(float temperature) {
+    char buffer[10];
+    dtostrf(temperature, 6, 2, buffer);
+
+    return String(buffer);
+  }
+
+  String Temperature(float temperature) {
+    char buffer[50];
+    char fmt[25] = " | temperature:'%s'";
+
+    sprintf(buffer, fmt, FloatToString(temperature).c_str());
+
+    return String(buffer);
+  }
+};
 // DEBUG LOGGING ---------------------------------------------------------------
 struct Debug {
 
   void LogCtxAndTemperature(DateTime now, String ctx, float temp) {
-    char buffer[100];
-    char tempBuffer[10];
-    dtostrf(temp, 6, 2, tempBuffer);
-
-    char fmt[50] = "unix:'%lu' | context:'%s' | temperature:'%s'";
-    sprintf(buffer, fmt, now.unixtime(), ctx.c_str(), tempBuffer);
-
-    log(String(buffer));
+    fmt fmt;
+    log(fmt.TimestampAndDate(now) + fmt.Context(ctx) + fmt.Temperature(temp));
   }
 
   void LogCtx(DateTime now, String ctx) {
-    char buffer[100];
-
-    char fmt[50] = "unix:'%lu' | context:'%s'";
-    sprintf(buffer, fmt, now.unixtime(), ctx.c_str());
-
-    log(String(buffer));
+    fmt fmt;
+    log(fmt.TimestampAndDate(now) + fmt.Context(ctx));
   }
 
   void log(String msg) {
@@ -96,24 +138,72 @@ void WriteTemperature(float temperature) {
   // SD CARD WRITE STUB
 }
 
+// RTC -------------------------------------------------------------------------
+void setupRTC() {
+  if (!rtc.begin()) {
+    error("RTC failed");
+  }
+
+  if (!rtc.initialized() || rtc.lostPower()) {
+    Serial.println("RTC is not initialized... setting time");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+
+  rtc.start();
+
+  float drift = 43;
+  float period_sec = (7 * 86400);
+  float deviation_ppm = (drift / period_sec * 1000000);
+  float drift_unit = 4.34;
+
+  int offset = round(deviation_ppm / drift_unit);
+
+  Serial.print("Offset is ");
+  Serial.println(offset);
+}
+
+String toHumanReadableDate(DateTime now) {
+  char buffer[100];
+  char fmt[50] = "%d/%02d/%02d %s %02d:%02d:%02d";
+
+  sprintf(buffer, fmt, now.year(), now.month(), now.day(),
+          DAYS_OF_THE_WEEK[now.dayOfTheWeek()], now.hour(), now.minute(),
+          now.second());
+
+  return String(buffer);
+}
+
+// SD --------------------------------------------------------------------------
+// void setupSD() {
+//   if (!SD.begin(10)) {
+//     error("Card failed, or not present");
+//   }
+//   Serial.println("SD initialized");
+
+//   char filename[] = "LOGS.CSV";
+//   if (!SD.exists(filename)) {
+//     logfile = SD.open(filename, FILE_WRITE);
+//   }
+
+//   if (!logfile) {
+//     error("couldnt create file");
+//   }
+// }
+
 // SETUP -----------------------------------------------------------------------
 void setup(void) {
   pinMode(NOTIFY_LED, OUTPUT);
+  pinMode(10, OUTPUT);
 
-  if (!rtc.begin()) {
-    Serial.println("RTC failed");
-  }
-
-  if (!rtc.isrunning()) {
-    Serial.println("RTC is not running");
-  }
+  setupRTC();
+  // setupSD();
 
   DEBUG_LOG_OUTPUT = DEBUG_LOG_SERIAL;
 }
 
 // LOOP ------------------------------------------------------------------------
 void loop(void) {
-  Serial.begin(9600);
+  Serial.begin(57600);
   Debug debug;
 
   float temperature = GetTemperature();
